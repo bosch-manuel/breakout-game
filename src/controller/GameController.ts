ce7a6ccbffbc20d9a70cee7819e4ee844fd4664e
Manuel Boesch <manuel.s.boesch@gmail.com>
2017-06-25 20:15:15 +0200

    ball initialization

--- src/controller/GameController.ts
+++ src/controller/GameController.ts
@@ -1,10 +1,24 @@
 import { Ball } from "../model/Ball";
 import { IBallView } from "../view/ball/IBallView";
+import { HTMLBall } from "../view/ball/HTMLBall";
+import { Point } from "../model/Point";
 
-export class GameControler {
+export class GameController {
     private ball: Ball;
     private ball_view: IBallView;
+    private _mainCanvas: HTMLCanvasElement;
+
     constructor(mainCanvas: HTMLCanvasElement) {
-        this.ball = new Ball()
+        this._mainCanvas = mainCanvas;
+        this.ball = this.createBall();
+        this.ball_view = this.initializeBallView(this._mainCanvas.getContext("2d"), this.ball);
+    }
+
+    private createBall(): Ball {
+        return new Ball(50, new Point(this._mainCanvas.width / 2, this._mainCanvas.height / 2));
+    }
+
+    private initializeBallView(context: CanvasRenderingContext2D, ball: Ball): IBallView {
+        return new HTMLBall(context, ball);
     }
 }
